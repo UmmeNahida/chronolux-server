9e9f098e4e0f4f5f02c1da37ce5efdadc32c4286
Nahida Akther <ummenahida20@gmail.com>
2026-05-04 21:52:30 +0600

    refactor: simplify environment variable loading using envalid

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,33 +1,9 @@
-import dotenv from "dotenv";
 
-dotenv.config();
+import "dotenv/config" 
+import { cleanEnv, str,num } from 'envalid'
 
+export const envVars = cleanEnv(process.env, {
+  port: num(),
+  DB_Url: str(),
+})
 
-
-interface IEnvVars {
-  DB_Url: string;
-  port: string;
-  node_env: string;
-  DB_USER:string;
-  DB_PASS:string
-}
-
-const loadEnvVars = (): IEnvVars => {
-  const requiredEnvVars: string[] = ["port", "DB_Url", "node_env","DB_USER","DB_PASS"];
-
-  requiredEnvVars.forEach((key) => {
-    if (!process.env[key]) {
-      throw new Error(`Missing require environment variable ${key}`);
-    }
-  });
-
-  return {
-    port: process.env.port as string,
-    DB_Url: process.env.DB_URL!,
-    node_env: process.env.node_env as string,
-    DB_USER: process.env.DB_USER as string,
-    DB_PASS: process.env.DB_PASS as string
-  };
-};
-
-export const envVars = loadEnvVars()
