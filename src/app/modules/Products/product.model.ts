951732d204ffc35d6949835f102c92484012e862
Nahida Akther <ummenahida20@gmail.com>
2026-05-06 16:41:10 +0600

    feat: update product schema to include additional fields for date, image, brand, and model

--- src/app/modules/Products/product.model.ts
+++ src/app/modules/Products/product.model.ts
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
   name: String,
-  type: String,
+  date: String,
   price: Number,
   description: String,
   category: String,
+  image: String,
+  brand: String,
+  model: String
 }, { timestamps: true });
 
 export const Product = mongoose.model("Product", productSchema);
