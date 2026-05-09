ef51c55c0bf7fa597cf2d9b9410342ab08a8ce7c
Nahida Akther <ummenahida20@gmail.com>
2026-05-10 03:03:13 +0600

    feat: import passport from config when app starts

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -1,71 +1,72 @@
-
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
-import httpStatus from "http-status-codes"
+import httpStatus from "http-status-codes";
 import AppError from "../../ErrorHandler/appErrors";
 import { createUserTokens } from "../../utils/userToken";
 import { authService } from "./auth.services";
 import { setAuthCookie } from "../../utils/setCookie";
 import passport from "passport";
 
-const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const user = await authService.addUser(req.body)
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await authService.addUser(req.body);
 
     sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.CREATED,
-        message: "user created successfully",
-        data: user
-    })
-})
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "user created successfully",
+      data: user,
+    });
+  },
+);
 
-const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
-    passport.authenticate("local", (err: any, user: any, info: any) => {
-
+const credentialsLogin = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate(
+      "local",
+      (err: any, user: any, info: any) => {
         if (err) {
-            return next(new AppError(401, info?.message || err));
+          return next(new AppError(401, info?.message || err));
         }
 
         if (!user) {
-            return next(new AppError(401, info?.message || "User does not exist"));
+          return next(
+            new AppError(401, info?.message || "User does not exist"),
+          );
         }
 
-        const userToken = createUserTokens(user)
+        const userToken = createUserTokens(user);
         //    console.log("userToken",userToken)
 
-
-        setAuthCookie(res, userToken)
+        setAuthCookie(res, userToken);
 
         // remove password from user object
-        delete user.toObject().password;
+        const userObj = user.toObject();
+        delete userObj.password;
 
-        sendResponse(res, {
-            success: true,
-            statusCode: httpStatus.OK,
-            message: "user login successfully",
-            data: {
-                accessToken: userToken.accessToken,
-                refreshToken: userToken.refreshToken,
-                user: user
-            }
-
-        })
-
-    }
-
-    )(req, res, next)
+        return sendResponse(res, {
+          success: true,
+          statusCode: httpStatus.OK,
+          message: "user login successfully",
+          data: {
+            accessToken: userToken.accessToken,
+            refreshToken: userToken.refreshToken,
+            user: user,
+          },
+        });
+      },
+    )(req, res, next);
     // console.log(users)
-})
+  },
+);
 
-
-const logout = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
+const logout = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     // res.clearCookie("token", {
     //     httpOnly: true,
     //     secure: true,
-    //     sameSite: 
+    //     sameSite:
     // })
 
     // res.clearCookie("refreshToken", {
@@ -75,47 +76,52 @@ const logout = catchAsync(async (req: Request, res: Response, next: NextFunction
     // })
 
     res.clearCookie("accessToken", {
-        httpOnly: true,
-        secure: true,      
-        sameSite: "none",   
-    })
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
 
     res.clearCookie("refreshToken", {
-        httpOnly: true,
-        secure: true,     
-        sameSite: "none",    
-    })
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
 
     sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.OK,
-        message: "user logout successfully",
-        data: {}
-    })
-})
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "user logout successfully",
+      data: {},
+    });
+  },
+);
 
-
-const getRefreshToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const getRefreshToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies.token;
     if (!token) {
-        throw new AppError(httpStatus.BAD_REQUEST, "token is not found")
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        "token is not found",
+      );
     }
-    const loginInfoDecoded = await authService.getNewRefreshToken(token as string)
+    const loginInfoDecoded = await authService.getNewRefreshToken(
+      token as string,
+    );
 
     sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.OK,
-        message: "user login successfully",
-        data: loginInfoDecoded
-
-    })
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "user login successfully",
+      data: loginInfoDecoded,
+    });
     // console.log(users)
-})
+  },
+);
 
-
-export const authController ={
-    createUser,
-    credentialsLogin,
-    getRefreshToken,
-    logout
-}
+export const authController = {
+  createUser,
+  credentialsLogin,
+  getRefreshToken,
+  logout,
+};
