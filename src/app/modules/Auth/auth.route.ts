b632005b15cb4c3f8281c8bd4e1675c19cfcd783
Nahida Akther <ummenahida20@gmail.com>
2026-05-09 21:18:42 +0600

    fix: correct route definition for user registration

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -5,7 +5,7 @@ import { authController } from "./auth.controller";
 
 
 const route = Router()
-
+route.post('/register', authController.createUser);
 route.post("/login", authController.credentialsLogin)
 route.post("/refresh-token", authController.getRefreshToken)
 route.post("/logout",authController.logout)
