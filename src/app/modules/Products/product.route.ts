6b83dfa74070267cc34ae2ccf744133a4decbb72
Nahida Akther <ummenahida20@gmail.com>
2026-05-12 01:12:33 +0600

    fix: update product routes for consistency and clarity

--- src/app/modules/Products/product.route.ts
+++ src/app/modules/Products/product.route.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { checkAuth } from "../../utils/checkAuth";
-import { createProduct, deleteProduct, getSingleProduct, updateProduct } from "./product.controller";
+import { createProduct, deleteProduct, getProducts, getSingleProduct, updateProduct } from "./product.controller";
 
 
 
 const router = Router();
 
 router.post('/create',createProduct)
-router.get("/products/:id", getSingleProduct);
-router.patch("/products/:id", checkAuth("admin"), updateProduct);
-router.delete("/products/:id", checkAuth("admin"), deleteProduct);
+router.post('/',getProducts)
+router.get("/details/:id", getSingleProduct);
+router.patch("/update/:id", checkAuth("admin"), updateProduct);
+router.delete("/delete/:id", checkAuth("admin"), deleteProduct);
 
 export const productRouter = router;
 
