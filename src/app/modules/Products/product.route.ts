3960921ac4cfe4a24f744a25da50f71df563aa76
Nahida Akther <ummenahida20@gmail.com>
2026-05-11 23:22:53 +0600

    feat: add create product path in router

--- src/app/modules/Products/product.route.ts
+++ src/app/modules/Products/product.route.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { checkAuth } from "../../utils/checkAuth";
-import { deleteProduct, getSingleProduct, updateProduct } from "./product.controller";
+import { createProduct, deleteProduct, getSingleProduct, updateProduct } from "./product.controller";
 
 
 
 const router = Router();
 
-router.post('/create',)
+router.post('/create',createProduct)
 router.get("/products/:id", getSingleProduct);
 router.patch("/products/:id", checkAuth("admin"), updateProduct);
 router.delete("/products/:id", checkAuth("admin"), deleteProduct);
