bec970b53604ff45a1270dbd02119d40229f54d5
Nahida Akther <ummenahida20@gmail.com>
2026-05-13 11:16:32 +0600

    feat: add product interface

---
+++ src/app/modules/Products/product.interface.ts
@@ -0,0 +1,12 @@
+export interface productInterface {
+  name: string
+  date: string
+  price: number
+  description: string
+  category: string
+  image: string
+  brand: string
+  model: string
+  rating: number
+  review: string
+}
